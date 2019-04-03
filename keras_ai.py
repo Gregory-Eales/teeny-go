f08713baa3ea9c2578bcd1f9321da25ed0bb3371
Gregory Eales <gregoeales@aol.com>
2019-04-02 17:29:56 -0700

    Update keras_ai.py

--- keras_ai.py
+++ keras_ai.py
@@ -4,23 +4,26 @@ from SGF_Loader import get_data
 from OptimizedGoTrainer import GoTrainer 
 import os
 from tqdm import tqdm
+import numpy as np
 
 GT = GoTrainer()
 
 #create model
 model = Sequential()
 #add model layers
-model.add(Conv2D(64, kernel_size=3, activation='tanh', padding="same", input_shape=(9,9,1)))
-model.add(Conv2D(32, kernel_size=3, activation='tanh', padding="same"))
+model.add(Conv2D(10, kernel_size=2, activation='relu', padding="same", input_shape=(9,9,1)))
+model.add(Conv2D(10, kernel_size=2, activation='relu', padding="same"))
 model.add(Flatten())
 model.add(Dense(81, activation='softmax'))
+model.add(Dense(81, activation='softmax'))
+
 
 #compile model using accuracy to measure model performance
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
 # get data file paths
 for path in os.walk("GoSampleData"):
-    paths = (path[2][0:10])
+    paths = (path[2][0:1])
 
 print("Playing Out Games: ")
 for i in tqdm(range(len(paths))):
@@ -28,14 +31,21 @@ for i in tqdm(range(len(paths))):
     GT.play(data=data, winner=winner)
 
 print("Training Teeny Go Neural Net: ")
-for i in range(1000):
+X_train = []
+y_train = []
+for i in range(10000):
     for j in range(len(GT.x_data)):
+    	
+    	X_train.append(GT.x_data[j].reshape(9, 9, 1))
+    	y_train.append(GT.y_data[j].reshape(81))
+    	
 
-        X_train = GT.x_data[j].reshape(9, 9)
-        y_train = GT.y_data[j].reshape(9, 9)
-        #train the model
-        model.fit(X_train, y_train, epochs=3)
+X_train = np.array(X_train)
+y_train = np.array(y_train)
 
+print(X_train.shape)
+print(y_train.shape)
 
+model.fit(X_train, y_train, epochs=1)
 
 
