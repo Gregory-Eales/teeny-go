78dafd488ca986153f829729ded53e2cbfb3a568
Gregory Eales <gregoeales@aol.com>
2019-08-20 00:06:38 -0700

    added network files

---
+++ teeny_go.py
@@ -0,0 +1,25 @@
+import torch
+
+class TeenyGo(object):
+
+
+    # - network to pick best move
+    # - network to predict winner
+    # - tree search method
+    # - get move method
+    # - internal go engine for tree search
+    # - internal board memory
+
+
+
+
+    def __init__(self):
+
+        self.value_network = None
+        self.advantage_network = None
+
+    def make_move(self):
+        pass
+
+    def save(self):
+        pass
