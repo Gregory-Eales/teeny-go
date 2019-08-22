7f4d4a257332f4d2173d7be4cfafb4212eaac9d1
Gregory Eales <gregoeales@aol.com>
2019-08-21 22:44:53 -0700

    Update teeny_go_network.py

--- teeny_go/teeny_go_network.py
+++ teeny_go/teeny_go_network.py
@@ -34,6 +34,34 @@ class Block(torch.nn.Module):
         out = self.relu2(out)
         return out
 
+
+class ValueHead(torch.nn.Module):
+
+    def __init__(self, num_channel):
+        super(ValueHead, self).__init__()
+        self.pad = torch.nn.ZeroPad2d(1)
+        self.conv = torch.nn.Conv2d(num_channel, num_channel, kernel_size=1)
+        self.batch_norm = torch.nn.BatchNorm2d(num_channel)
+        self.relu1 = torch.nn.ReLU()
+        self.fc1 = torch.nn.Linear(256, 256)
+        self.relu2 = torch.nn.ReLU()
+        self.fc2 = torch.nn.linear(256, 1)
+        self.tanh = torch.nn.Tanh
+
+    def forward(self, x):
+
+        out = self.pad(x)
+        out = self.conv(x)
+        out = self.batch_norm(x)
+        out = self.relu1(x)
+        out = out.reshape(out.size(0), -1)
+        out = self.fc1(out)
+        out = self.relu2(out)
+        out = self.fc2(out)
+        out = self.tanh(out)
+
+        return out
+
 class TeenyGoNetwork(torch.nn.Module):
 
     # convolutional network
@@ -51,6 +79,7 @@ class TeenyGoNetwork(torch.nn.Module):
         self.input_channels = input_channels
         self.res_layers = {}
         self.optimizer = None
+        self.loss = None
 
         # initilize network
         self.pad = torch.nn.ZeroPad2d(1)
@@ -62,7 +91,7 @@ class TeenyGoNetwork(torch.nn.Module):
         self.initialize_optimizer()
 
 
-    def predict(self, x):
+    def forward(self, x):
 
         out = self.pad(x)
         out = self.conv(out)
@@ -76,21 +105,32 @@ class TeenyGoNetwork(torch.nn.Module):
 
 
     def initialize_layers(self):
-
-
         for i in range(1, self.num_res_blocks+1):
             self.res_layers["l"+str(i)] = Block(self.num_channels)
 
-    def initialize_optimizer(self):
-        pass
+    def initialize_optimizer(self, learning_rate=0.01):
+        #Loss function
+        self.loss = torch.nn.CrossEntropyLoss()
+
+        #Optimizer
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=learning_rate)
+
+    def optimize(self, x, y, iterations=10):
+
+        for iter in range(iterations):
+            optimizer.zero_grad()   # zero the gradient buffers
+            output = self.forward(input)
+            loss = self.loss(output, target)
+            loss.backward()
+            optimizer.step()
 
 
 
 def main():
-    x = torch.randn(100, 11, 9, 9)
-    tgn = TeenyGoNetwork(num_res_blocks=10)
-    tgn.predict(x)
-    
+    x = torch.randn(20, 11, 9, 9)
+    tgn = TeenyGoNetwork(num_res_blocks=5)
+    print(tgn(x)).shape
+
 
 if __name__ == "__main__":
     main()
