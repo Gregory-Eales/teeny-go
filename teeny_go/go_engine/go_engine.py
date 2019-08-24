43dbafa71d09fcbc7e44c96e9f6a8fe439bd379b
Gregory Eales <gregoeales@aol.com>
2019-08-24 03:09:33 -0700

    Update go_engine.py

--- teeny_go/go_engine/go_engine.py
+++ teeny_go/go_engine/go_engine.py
@@ -5,4 +5,30 @@ import numpy as np
 class GoEngine(self):
 
     def __init__(self):
-        pass
+
+        # initialize game attributes
+        self.board = None
+        self.turn = None
+        self.white_score = None
+        self.black_score = None
+        self.playing = None
+        self.deciding_move = True
+
+    def new_game(self):
+        self.board = self.create_board()
+        self.turn = "black"
+        self.white_score = 0
+        self.black_score = 0
+        self.playing = True
+        self.deciding_move = True
+
+    def create_board(self):
+        return np.zeros([9, 9])
+
+    def play(self):
+
+        while self.playing:
+
+            while self.deciding_move:
+
+                pass
