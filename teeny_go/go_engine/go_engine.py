a95a3425c8dd99f6e64076e6751dced393566f01
Gregory Eales <33496134+Gregory-Eales@users.noreply.github.com>
2019-08-26 15:37:36 -0700

    Update go_engine.py

--- teeny_go/go_engine/go_engine.py
+++ teeny_go/go_engine/go_engine.py
@@ -18,6 +18,8 @@ class GoEngine(object):
         self.end_score = None
         self.playing = None
         self.turn_to_num = {"white": -1, "black": 1}
+        self.board_cache = None
+        self.new_game()
 
     def new_game(self):
         self.board = self.create_board()
@@ -25,9 +27,14 @@ class GoEngine(object):
         self.white_score = 0
         self.black_score = 0
         self.end_score = 0
+        self.board_cache = []
 
     def create_board(self):
         return np.zeros([9, 9])
+
+    def print_board(self):
+        for _ in range(9):
+            print(self.board[_])
 
     #######################
     # Game Action Methods #
@@ -56,9 +63,12 @@ class GoEngine(object):
         self.board[move[1]][move[0]] = self.turn_to_num[self.turn]
 
     def get_pos_state(self, pos):
-        return self.board[move[1]][move[0]]
+        return self.board[pos[1]][pos[0]]
 
     def change_turn(self):
+
+        self.board_cache.append(self.board)
+
         if self.turn == "black":
             self.turn == "white"
         else:
@@ -70,26 +80,148 @@ class GoEngine(object):
 
     def move_is_valid(self, move):
 
+        valid = False
+
         # check if space is empty
         if self.get_pos_state(move) != 0:
             return False
 
+        else:
+            self.make_move(move)
+
         # check if has liberties
         if self.has_liberties(move) == True:
-            return True
+            valid = True
 
         # if no liberties check if capturing enemy
-        if self.is_capturing_enemy(move)== True:
-            self.capture_enemies(move)
-            return True
+        if self.is_capturing_enemy(move) == True:
+            self.capture_enemy(move)
+            valid = True
 
         # get group
         group = self.get_group(move)
 
-        # if no liberties, check if group
+        # check if group has liberties
+        if self.check_group_liberties(group) == True:
+            return True
+
         # and if group has liberties
 
+
         # if group
+
+
+        if valid == False:
+            self.board = self.board_cache[-1]
+            return False
+
+        else:
+            return True
+
+    def has_existed(self):
+
+        for board in self.board_cache:
+            if board == self.board:
+                return True
+        else:
+            return False
+
+    def has_liberties(self, loc):
+
+        if loc[0] > 0 and loc[0] < 8 and loc[1] > 0 and loc[1] < 8:
+
+            if self.get_pos_state([loc[1]-1, loc[0]]) == 0:
+                return True
+
+            if self.get_pos_state([loc[1]+1, loc[0]]) == 0:
+                return True
+
+            if self.get_pos_state([loc[1], loc[0]-1]) == 0:
+                return True
+
+            if self.get_pos_state([loc[1], loc[0]+1]) == 0:
+                return True
+
+        if loc[0] != 0:
+            if self.get_pos_state([loc[1], loc[0]-1]) == 0:
+                return True
+
+        if loc[0] != 8:
+            if self.get_pos_state([loc[1], loc[0]+1]) == 0:
+                return True
+
+        if loc[1] != 0:
+            if self.get_pos_state([loc[1]-1, loc[0]]) == 0:
+                return True
+
+        if loc[1] != 8:
+            if self.get_pos_state([loc[1]+1, loc[0]]) == 0:
+                return True
+
+        return False
+
+    def get_near(self, loc, type):
+
+        near = []
+
+        if loc[0] > 0 and loc[0] < 8 and loc[1] > 0 and loc[1] < 8:
+
+            if self.get_pos_state([loc[1]-1, loc[0]]) == type:
+                near.append([loc[0], loc[1]-1])
+
+            if self.get_pos_state([loc[1]+1, loc[0]]) == type:
+                near.append([loc[0], loc[1]+1])
+
+            if self.get_pos_state([loc[1], loc[0]-1]) == type:
+                near.append([loc[0]-1, loc[1]])
+
+            if self.get_pos_state([loc[1], loc[0]+1]) == type:
+                near.append([loc[0]+1, loc[1]-1])
+
+        if loc[0] != 0:
+            if self.get_pos_state([loc[1], loc[0]-1]) == type:
+                near.append([loc[0]-1, loc[1]])
+
+        if loc[0] != 8:
+            if self.get_pos_state([loc[1], loc[0]+1]) == type:
+                near.append([loc[0]+1, loc[1]])
+
+        if loc[1] != 0:
+            if self.get_pos_state([loc[1]-1, loc[0]]) == type:
+                near.append([loc[0], loc[1]-1])
+
+        if loc[1] != 8:
+            if self.get_pos_state([loc[1]+1, loc[0]]) == type:
+                near.append([loc[0], loc[1]+1])
+
+        if near != []:
+            return near
+        else:
+            return False
+
+    def is_capturing_enemy(self, loc):
+        pass
+
+    def capture_enemy(self, loc):
+        pass
+
+    def get_group(self, loc):
+        type = self.board[loc[1]][loc[0]]
+        group = []
+        searching = True
+        # if near spots = same type add to group
+        near = self.get_near(loc, type)
+        while searching:
+            searching = False
+            for space in near:
+
+
+
+
+
+    def check_group_liberties(self, group):
+        pass
+
 
 def create_board():
     board = []
@@ -110,7 +242,8 @@ def create_board():
 def main():
     engine = GoEngine()
     engine.board = create_board()
-    print(engine.board)
+    engine.print_board()
+    print(engine.has_liberties([2, 0]))
 
 if __name__ == "__main__":
     main()
