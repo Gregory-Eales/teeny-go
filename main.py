91f68d25f1c8458afc386753e72c7594cf8de19c
Gregory Eales <33496134+Gregory-Eales@users.noreply.github.com>
2019-08-29 21:42:33 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,13 @@
 #import logging
 import torch
 from teeny_go.go_trainer import GoTrainer
+from teeny_go.teeny_go_network import TeenyGoNetwork
+
+
+def load_data():
+    pass
+
+
+
+tgn = TeenyGoNetwork()
+tgn.load_state_dict("Models\Model_R5_C64_V0.pt")
