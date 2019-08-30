a51ed647e1eebaaf8d90b568d85da3a8b11f1350
Gregory Eales <gregoeales@aol.com>
2019-08-29 20:45:48 -0700

    added multiengine

---
+++ teeny_go/go_engine/multi_go_engine.py
@@ -0,0 +1,12 @@
+from go_engine import GoEngine
+import numpy as np
+
+
+
+class MultiGoEngine(object):
+
+    def __init__(self):
+        pass
+
+    def get_input_vector(self, input):
+        self.input = input
