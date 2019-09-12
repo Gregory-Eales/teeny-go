b2b2caab97a397a6d64d8aea47b5292e45113ed9
gregory-eales <gregoeales@aol.com>
2019-09-11 20:53:58 -0700

    changed files

--- spiel.py
+++ spiel.py
@@ -1,5 +1,5 @@
-from teeny_go.go_engine.open_spiel.build_python_3.python import pyspiel as ps
-
+import pyspiel as ps
+import numpy as np
 import time
 
 
@@ -9,26 +9,47 @@ import time
 board_size = {"board_size": ps.GameParameter(9)}
 game = ps.load_game("go", board_size)
 
-state = game.new_initial_state()
+state1 = game.new_initial_state()
+state2 = game.new_initial_state()
 
-print(state.information_state())
+print(state1.information_state())
+print(state1.information_state_as_normalized_vector(), "yes")
 
 is_playing = True
 t = time.time()
 
+board = []
 
+for i in range(9):
+    board.append(np.zeros([9,9]))
 
 for i in range(150):
 
+    black = []
+    white = []
+    turn = None
+    print(state1.current_player())
+    if turn == "white":
+        turn = [np.zeros([9, 9])]
+    else:
+        turn = [np.ones([9, 9])]
 
-    legal_actions = state.legal_actions()
+    for i in range(1, 6):
+        black.append(np.where(board[-i] == 1, 1, 0))
+        white.append(np.where(board[-i] == -1, 1, 0))
+    a = np.array(black+white+turn)
+
+    legal_actions = state1.legal_actions()
     #print(legal_actions)
     if len(legal_actions) > 4:
-        state.apply_action(legal_actions[-1])
+        state1.apply_action(legal_actions[-1])
     else:
         is_playing = False
 
+    board.append(np.array(state1.information_state_as_normalized_vector()))
 
-#print(state.history())
 
-print(1/(time.time()-t), "games/second")
+
+
+
+print(1/60*(time.time()-t), "games/minute")
