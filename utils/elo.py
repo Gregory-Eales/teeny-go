8c2711997477241276e6d9104edce4d7a64ceb72
Gregory Eales <gregoeales@aol.com>
2019-09-13 12:04:22 -0700

    Update elo.py

---
+++ utils/elo.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class EloRater(object):
+    
+    def __init__(self):
+        self.rating = None
+        
+    self.update_rating(self):
+        pass
+  
+    self.calculate_win_rate(self, elo1, elo2):
+        pass
+        
+    self.save_rating_data(self, rating_data):
+        pass
