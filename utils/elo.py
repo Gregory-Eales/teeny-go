70759c88b9fd440f25f7ca2e108dd2de1dd88000
Gregory Eales <gregoeales@aol.com>
2019-09-13 17:19:21 -0700

    Update elo.py

--- utils/elo.py
+++ utils/elo.py
@@ -1,15 +1,34 @@
+from matplotlib import pyplot as plt
 import numpy as np
 
+import logging
+
+
 class EloRater(object):
-    
+
     def __init__(self):
-        self.rating = None
+
+        # k factor is 16 for experts and 32 for weaker players
+        self.k_factor = 32
+
+    def calculate_expected_score(self, R1, R2):
+        return 1/(1+10**((R2-R1)/400))
+
+    def update_score(self, R1, R2, S):
+
+        # R1 = score to be updated
+        # R2 = opponent score
+        # S = actual score
+        # or percentage of wins against opponent
         
-    self.update_rating(self):
-        pass
-  
-    self.calculate_win_rate(self, elo1, elo2):
-        pass
+        expected_score = self.calculate_expected_score(R1, R2)
+        return R1 + self.k_factor*(S - expected_score)
         
-    self.save_rating_data(self, rating_data):
-        pass
+        
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
