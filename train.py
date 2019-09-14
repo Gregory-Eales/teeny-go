c5fd55a77d32ea87bf92b4d4853951d47a18bfad
Gregory Eales <gregoeales@aol.com>
2019-09-14 04:22:55 -0700

    Update train.py

--- train.py
+++ train.py
@@ -3,3 +3,8 @@ from teeny_go.teeny_go_network import TeenyGoNetwork
 
 
 tgn = TeenyGoNetwork(num_channels=32, num_res_blocks=5)
+tgn.double()
+
+gt = GoTrainer(network=tgn)
+
+gt.play_through_games(num_games=10)
