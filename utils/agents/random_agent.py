2c9ec5973a75a2d237ac1c0b28326d30f9e79b94
Gregory Eales <gregoeales@aol.com>
2019-09-13 18:22:36 -0700

    Update random_agent.py

--- utils/agents/random_agent.py
+++ utils/agents/random_agent.py
@@ -8,22 +8,9 @@ class RandomAgent(object):
 
     def make_move(self, valid_moves):
         return np.random.choice(list(range(82)),p=valid_moves)
-        
-        
-class MultiRandomAgent(object):
-
-    def __init__(self):
-        
-        self.elo_rating = 0
-
-    def make_move(self, valid_moves):
-        return np.random.choice(list(range(82)),p=valid_moves)
-
+    
     def make_moves(self, valid_moves):
         moves = []
         for move in range(valid_moves):
             moves.append(self.make_move(move))
-
         return moves
-        
-    
