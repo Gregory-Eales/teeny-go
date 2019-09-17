12a878bbd35e79efbb411a41ab2915e9b21a7926
Gregory Eales <gregoeales@aol.com>
2019-09-17 12:29:34 -0700

    test training run

--- train.py
+++ train.py
@@ -4,11 +4,11 @@ from teeny_go.teeny_go_network import TeenyGoNetwork
 import time
 
 
-tgn = TeenyGoNetwork(num_channels=32, num_res_blocks=3, is_cuda=True)
-tgn.cuda()
+tgn = TeenyGoNetwork(num_channels=32, num_res_blocks=3, is_cuda=False)
+#tgn.cuda()
 trainer = Trainer(network=tgn)
 
 t = time.time()
-trainer.train_self_play(num_games=5000, is_cuda=True, iterations=100)
+trainer.train_self_play(num_games=100, is_cuda=False, iterations=1)
 
 print("Played Through:", 1000, "games in", round(time.time()-t, 3), "seconds")
