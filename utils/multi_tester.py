8a978d101bab2837f05272e5eba705e5576f78e8
Gregory Eales <gregoeales@aol.com>
2019-09-20 09:47:32 -0700

    Update multi_tester.py

--- utils/multi_tester.py
+++ utils/multi_tester.py
@@ -18,10 +18,24 @@ class MultiTester(object):
         # initialize engine
         self.engine = MultiGoEngine()
 
+        # initialize elo agent rater
+        self.rater = Rater()
+
+        # initlize logger
+        self.logger = logging.getLogger("Multi-Agent-Tester")
+
+        # initilize something..
+
     # reset engine with n games
     def reset_engine(self, n=100):
         del(self.engine)
         self.engine = MultiGoEngine(num_games=n)
+
+    def make_move(self, ai):
+        state_tensor = (torch.from_numpy(self.engine.get_active_game_states())).cuda().type(torch.cuda.FloatTensor)
+        move_tensor = self.network.forward(state_tensor)
+        torch.cuda.empty_cache()
+        self.engine.take_game_step(move_tensor.cpu().detach().numpy())
 
     # plays through n games a1 vs a2
     def play_throgh_games(self, a1, a2, num_games):
@@ -30,4 +44,22 @@ class MultiTester(object):
         self.reset_engine(num_games)
 
         # play through games
-        
+
+        while self.engine.is_playing_games():
+
+            turn = self.engine.get_turn()
+
+            if turn == 0:
+                # get a1 moves
+                self.make_move(a1)
+
+            elif turn == 1:
+                # get a2 moves
+                self.make_move(a2)
+
+            # if game is over
+            elif turn == -4:
+                break
+
+    def get_win_rates(self):
+            pass
