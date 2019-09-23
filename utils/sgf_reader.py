b9e68d0981982dc65a4989277871bef91d48d317
Gregory Eales <gregoeales@aol.com>
2019-09-23 01:55:40 -0700

    Create sgf_reader.py

---
+++ utils/sgf_reader.py
@@ -0,0 +1,20 @@
+import numpy
+import pyspiel
+import torch
+
+
+
+class Reader(object):
+
+    def __init__(self):
+        pass
+
+
+    def load_file(self):
+        pass
+
+    def convert_file(self):
+        pass
+
+    def create_board_tensor(self):
+        pass
