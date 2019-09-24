437c3c55f727faaed08f78086b6c032ff761ae94
gregory-eales <gregoeales@aol.com>
2019-09-24 14:17:32 -0700

    added tensor generation

--- utils/sgf_reader.py
+++ utils/sgf_reader.py
@@ -12,10 +12,10 @@ class Reader(object):
 
         # init translation dict
         self.letter_to_number = {}
-        self.letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
+        self.letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "t"]
 
         # populate translation dict
-        for i in range(9):
+        for i in range(10):
             self.letter_to_number[self.letters[i]] = i
 
         # initilize game state
@@ -23,13 +23,13 @@ class Reader(object):
 
         self.move_map = self.get_move_map()
 
-    def generate_date(self, paths):
+    def generate_date(self, paths, dest_path):
 
         # reset generator
         self.initialize_game_state()
 
         # loop through paths:
-        for path in tqdm(paths):
+        for j, path in tqdm(enumurate(paths)):
         #   load file at path
             file = open(path, mode='r')
         #   read file
@@ -37,18 +37,43 @@ class Reader(object):
         #   loop through lines
             for i, line in enumerate(lines):
 
+                # get winner
                 if i == 0:
                     outcome = line.split("RE[")[1].split("]")[0][0]
-                    print(outcome)
+                    if outcome == "d":
+                        winner = "draw"
+                    elif outcome == "w":
+                        winner = "white"
+                    elif outcome == "b":
+                        winner = "black"
 
-                if i!=0 and line!=" ":
+                # get and translate move
+                if i != 0:
                     loc = line[3:5]
-                    try: loc = [loc[0], loc[1]]
+                    try:
+                        x = self.letter_to_number[loc[0]]
+                        y = self.letter_to_number[loc[1]]
+                        move = x + 9*y
+                        if move == 90:
+                            move = 81
+
+                        self.update_board()
+                        self.move_states.append(self.generate_move_tensor(move, winner))
+                        self.games_states.append(self.generate_move_tensor())
+                        # make move
+                        move = self.move_map[move]
+
+
                     except: pass
-                    for m in loc:
-                        if m not in letters:
-                            letters.append(m)
-                    print(loc)
+
+            # convert tenors lists to tensors
+
+            # convert numpy tensors to torch tensors
+
+            # save tensors in data folder
+
+
+
 
 
         #   for move in moves in file:
@@ -57,6 +82,12 @@ class Reader(object):
         #       make and save board tensor
 
         pass
+
+    def update_board(self):
+        state = self.board_state.observation_as_normalized_vector()
+        state = np.array(state).reshape(-1, 81)
+        state = (state[0] + state[1]*-1)
+        self.game_states.append(np.copy(state.reshape(1, 9, 9)))
 
     def get_move_map(self):
         board_size = {"board_size": pyspiel.GameParameter(9)}
@@ -70,7 +101,26 @@ class Reader(object):
         game = pyspiel.load_game("go", board_size)
         self.board_state = game.new_initial_state()
         self.game_states = []
+        self.move_states = []
         for i in range(7): self.game_states.append(np.zeros([9,9]))
+
+    def generate_move_tensor(self, move, winner):
+        turn = self.board_state.current_player()
+
+        move_tensor = np.zeros([1, 82])
+
+        if turn == 0 and winner == "black":
+            outcome = 1
+
+        elif turn == 1 and winner == "white":
+            outcome = 1
+
+        elif winner == "draw":
+            outcome = 0
+
+        else: outcome = -1
+
+        move_tensor[0][81] = outcome
 
     def generate_state_tensor(self):
 
