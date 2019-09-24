dd31370e80651dac2f717cdaa3d37a9208e82425
Gregory Eales <gregoeales@aol.com>
2019-09-24 11:41:46 -0700

    Update process_data.py

--- process_data.py
+++ process_data.py
@@ -17,16 +17,26 @@ for i in range(19, 20):
 
 t = time.time()
 
+letters = []
+
 for path in file_paths[0:10]:
     file = open(path, mode='r')
 
     lines = file.readlines()
 
     for i, line in enumerate(lines):
+
         if i == 0:
             outcome = line.split("RE[")[1].split("]")[0][0]
             print(outcome)
-        else:
+
+        if i!=0 and line!=" ":
             loc = line[3:5]
-            loc = [loc[0], loc[1]]
+            try: loc = [loc[0], loc[1]]
+            except: pass
+            for m in loc:
+                if m not in letters:
+                    letters.append(m)
             print(loc)
+
+print(letters)
