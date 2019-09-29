028aaad6ef712dacb4db44896687104782b6f29e
Gregory Eales <gregoeales@aol.com>
2019-09-29 16:24:59 -0700

    added elo storage file

--- utils/rater.py
+++ utils/rater.py
@@ -1,16 +1,28 @@
 import logging
+import os
 
 from matplotlib import pyplot as plt
 import numpy as np
+import pandas as pd
 
 
 class Rater(object):
 
-    def __init__(self):
+    def __init__(self, k_factor=32):
 
         # k factor is 16 for experts and 32 for weaker players
         # determines amount that scores are updated
         self.k_factor = 32
+        self.file_name = "elo.csv"
+        self.file_path = "data/"
+        self.file = read_elo_file()
+
+    def read_elo_file(self):
+        return pd.load_csv(self.file_path+self.file_name)
+
+    def initilize_file_path(self):
+        # use os lib to find file path relative to execution path
+        pass
 
     def calculate_expected_score(self, R1, R2):
         return 1/(1+10**((R2-R1)/400))
