50f428f8ba02f0217ff6294c78d18e05a95461a4
Gregory Eales <gregoeales@aol.com>
2019-09-30 16:07:05 -0700

    Update rater.py

--- utils/rater.py
+++ utils/rater.py
@@ -24,10 +24,29 @@ class Rater(object):
         # use os lib to find file path relative to execution path
         pass
 
+    def update_score(self, A1, A2, winner):
+
+        assert(type(A1) == string), "A1: agent type must string"
+        assert(type(A2) == string), "A2: agent type must string"
+        assert(type(winner) == string), "winner: winnner type must be string"
+
+        # get current score for each agent in csv file
+        A1_score = None
+        A2_score = None
+
+        # convert winner to numerical value
+        winner = None
+
+        # calculate updated score
+        A1_update = self.calculate_updated_score(A1_score, A2_score, winner)
+        A2_update = self.calculate_updated_score(A2_score, A1_score, winner)
+
+        # write new elo rating to csv file, save
+
     def calculate_expected_score(self, R1, R2):
         return 1/(1+10**((R2-R1)/400))
 
-    def update_score(self, R1, R2, S):
+    def calculate_updated_score(self, R1, R2, S):
 
         # R1 = score to be updated
         # R2 = opponent score
@@ -36,6 +55,12 @@ class Rater(object):
 
         expected_score = self.calculate_expected_score(R1, R2)
         return R1 + self.k_factor*(S - expected_score)
+
+    def initilize_elo_file(self):
+        # create new elo file
+
+        # check to to see if elo file is already in place
+        pass
 
 
 
