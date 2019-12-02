ac762221d90cd53d62c1e12aba5e99f0eed30676
Gregory Eales <gregoeales@aol.com>
2019-12-02 15:27:58 -0800

    Update policy_network.py

--- teeny_go/policy_network.py
+++ teeny_go/policy_network.py
@@ -27,18 +27,66 @@ class Block(torch.nn.Module):
 
 class PolicyNetwork(torch.nn.Module):
 
-    def __init__(self, alpha, num_res=3):
+    def __init__(self, alpha, num_res=3, num_channel=3):
         super(PolicyNetwork, self).__init__()
 
+        self.input_channels = num_channel
+        self.num_res = num_res
+        self.res_block = {}
+        self.num_channel = 11
+
+        self.define_network()
         # define optimizer
         self.optimizer = torch.optim.Adam(lr=alpha, params=self.parameters())
 
         self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu:0')
         self.to(self.device)
 
+    def define_network(self):
+        #policy head
+        self.policy_conv = torch.nn.Conv2d(self.num_channel, 2, kernel_size=1)
+        self.policy_batch_norm = torch.nn.BatchNorm2d(self.num_channel)
+        self.relu = torch.nn.ReLU()
+        self.policy_fc = torch.nn.Linear(self.num_channel*9*9, 82)
+        self.softmax = torch.nn.Softmax()
+
+        # network start
+        self.pad = torch.nn.ZeroPad2d(1)
+        self.conv = torch.nn.Conv2d(11, self.num_channel, kernel_size=3)
+        self.batch_norm = torch.nn.BatchNorm2d(self.num_channel)
+        self.relu = torch.nn.ReLU()
+
+        for i in range(1, self.num_res+1):
+            self.res_block["r"+str(i)] = Block(self.num_channel)
+
     def forward(self, x):
         out = torch.Tensor(x).to(self.device)
+
+        out = self.pad(x)
+        out = self.conv(out)
+        out = self.batch_norm(out)
+        out = self.relu(out)
+
+        for i in range(1, self.num_res+1):
+            out = self.res_block["r"+str(i)](out)
+
+        # policy head
+        out = self.policy_conv(x)
+        out = self.policy_batch_norm(x)
+        out = self.relu(x)
+        out = out.reshape(-1, self.num_channel*9*9)
+        out = self.policy_fc(out)
+        out = self.softmax(out)
+        return out
         pass
 
     def optimize(self):
         pass
+
+def main():
+    pn = PolicyNetwork(alpha=0.01, num_res=3, num_channel=3)
+
+    x = torch.ones(100, 11, 9, 9)
+    print(pn.forward(x).shape)
+
+if __name__ == "__main__": main()
