9f4548cbcc47cb36337890a67c83b136a44a9c23
Gregory Eales <gregoeales@aol.com>
2020-03-26 09:07:17 +0000

    Update train.py

--- train.py
+++ train.py
@@ -5,8 +5,7 @@ from matplotlib import pyplot as plt
 
 torch.cuda.empty_cache()
 
-value_net = ValueNetwork(alpha=0.005, num_res=3, num_channel=128)
-#value_net.load_state_dict(torch.load("models/value-net/VN-R5-C64-V1.pt"))
+value_net = ValueNetwork(alpha=0.001, num_res=8, num_channel=32)
 print(value_net.device)
 
 
@@ -15,7 +14,7 @@ y = []
 
 x_path = "data/aya_dataset/DataX"
 y_path = "data/aya_dataset/DataY"
-for i in range(2000):
+for i in range(100):
 
     x.append(torch.load(x_path+str(i)+".pt")[-20:])
     y.append(torch.load(y_path+str(i)+".pt")[-20:])
@@ -29,10 +28,13 @@ y = torch.cat(y).float()
 y = y[:,82].reshape(y.shape[0], -1)[rand_perm]
 
 
-value_net.optimize(x, y, batch_size=16, iterations=50, alpha=0.01)
+value_net.optimize(x, y, batch_size=64, iterations=3, alpha=0.01)
 
 
 plt.plot(value_net.historical_loss)
 plt.show()
 
 torch.save(value_net.state_dict(), "models/value-net/VN-R3-C128-V1.pt")
+
+
+# process dataset 
