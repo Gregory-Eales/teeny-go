db8a22d7eff06e95134bce002ecbebdc0488667d
Gregory Eales <gregoeales@aol.com>
2020-03-27 23:42:40 +0000

    Update process_data.py

--- process_data.py
+++ process_data.py
@@ -1,12 +1,22 @@
 import glob
+from os import listdir
 import time
 
-from utils.sgf_reader import Reader
+from utils.sgf_reader2 import Reader
 
 file_paths = []
 
-path = "data/aya_self_play"
+path = "data/cleaned_pro_games"
 
+file_paths = listdir(path)
+
+
+for i in range(len(file_paths)):
+    file_paths[i] = path+"/"+file_paths[i]
+
+print(len(file_paths))
+
+"""
 for i in range(1, 2):
     if i < 10:
         mypath = path + "/0" + str(i)
@@ -14,8 +24,8 @@ for i in range(1, 2):
         mypath = path + "/" + str(i)
 
     file_paths += glob.glob(mypath + "/*.sgf")
-
+"""
 
 sgfr = Reader()
 
-sgfr.generate_data(file_paths, "data/aya_dataset/")
+sgfr.generate_data(file_paths, "data/pro_game_dataset/")
