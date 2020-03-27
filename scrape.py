3412ff17722ba096b270748eea932f4bec9bcbda
Gregory Eales <gregoeales@aol.com>
2020-03-27 23:42:43 +0000

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -2,7 +2,4 @@ from utils.sgf_scraper import GoScraper
 
 
 gs = GoScraper()
-gs.get_leaderboard_users()
-print(len(gs.user_links))
-gs.save_user_links()
-print(len(gs.user_links))
+gs.read_user_links()
