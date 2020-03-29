7bfa838f25dde49b31b39da9dc4380d9d81fb76f
Gregory Eales <gregoeales@aol.com>
2020-03-29 21:07:23 +0100

    Update train.py

--- train.py
+++ train.py
@@ -5,20 +5,20 @@ from matplotlib import pyplot as plt
 
 torch.cuda.empty_cache()
 
-value_net = ValueNetwork(alpha=0.001, num_res=8, num_channel=32)
+value_net = ValueNetwork(alpha=0.001, num_res=5, num_channel=64)
 print(value_net.device)
 
 
 x = []
 y = []
 
-x_path = "data/aya_tensor_dataset/DataX"
-y_path = "data/aya_tensor_dataset/DataY"
-for i in range(100):
-
-    x.append(torch.load(x_path+str(i)+".pt")[-20:])
-    y.append(torch.load(y_path+str(i)+".pt")[-20:])
-
+x_path = "data/pro_game_dataset/DataX"
+y_path = "data/pro_game_dataset/DataY"
+for i in range(10):
+    try:
+        x.append(torch.load(x_path+str(i)+".pt"))
+        y.append(torch.load(y_path+str(i)+".pt"))
+    except:pass
 
 
 x = torch.cat(x).float()
@@ -27,6 +27,8 @@ x = x[rand_perm]
 y = torch.cat(y).float()
 y = y[:,82].reshape(y.shape[0], -1)[rand_perm]
 
+print(y.shape)
+print(x.shape)
 
 value_net.optimize(x, y, batch_size=64, iterations=3, alpha=0.01)
 
