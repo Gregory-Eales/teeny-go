13690829f26b34ae140a362b77c1a12643c5d9d4
Gregory Eales <gregoeales@aol.com>
2020-03-30 12:44:06 +0100

    Update train_value.py

--- train_value.py
+++ train_value.py
@@ -2,44 +2,47 @@ import torch
 from teeny_go.value_network import ValueNetwork
 from matplotlib import pyplot as plt
 
-
+"""
 for res in [3, 5, 8, 12]:
     for chan in [16, 32, 64, 128, 256]:
+        pass
+"""
 
-        torch.cuda.empty_cache()
+torch.cuda.empty_cache()
 
-        value_net = ValueNetwork(alpha=0.001, num_res=res, num_channel=chan)
-        print(value_net.device)
-
-
-        x = []
-        y = []
-
-        x_path = "data/pro_game_dataset/DataX"
-        y_path = "data/pro_game_dataset/DataY"
-        for i in range(10):
-            try:
-                x.append(torch.load(x_path+str(i)+".pt"))
-                y.append(torch.load(y_path+str(i)+".pt"))
-            except:pass
+value_net = ValueNetwork(alpha=0.0001, num_res=5, num_channel=128)
+print(value_net.device)
 
 
-        x = torch.cat(x).float()
-        rand_perm = torch.randperm(x.shape[0])
-        x = x[rand_perm]
-        y = torch.cat(y).float()
-        y = y[:,82].reshape(y.shape[0], -1)[rand_perm]
+x = []
+y = []
 
-        print(y.shape)
-        print(x.shape)
-
-        value_net.optimize(x, y, batch_size=64, iterations=3, alpha=0.01)
-
-
-        plt.plot(value_net.historical_loss)
-        plt.show()
-
-        torch.save(value_net.state_dict(), "models/value-net/VN-R3-C128-V1.pt")
+x_path = "data/ogs_tensor_games/DataX"
+y_path = "data/ogs_tensor_games/DataY"
+for i in range(75):
+    try:
+        x.append(torch.load(x_path+str(i)+".pt"))
+        y.append(torch.load(y_path+str(i)+".pt"))
+    except:pass
 
 
-        # process dataset
+x = torch.cat(x).float()
+rand_perm = torch.randperm(x.shape[0])
+x = x[rand_perm]
+y = torch.cat(y).float()
+y = y[:,82].reshape(y.shape[0], -1)[rand_perm]
+
+print(x.shape[0])
+
+ts = int(x.shape[0]*0.8)
+
+x_t, y_t = x[ts:], y[ts:]
+x, y = x[0:ts], y[0:ts]
+
+value_net.optimize(x, y, x_t, y_t, batch_size=64,
+ iterations=3, alpha=0.01, test_interval=5)
+
+plt.plot(value_net.historical_loss)
+plt.plot(value_net.test_accuracies)
+plt.plot(value_net.test_losses)
+plt.show()
