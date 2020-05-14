df8540655379ee4cf6d57e486e649fba33b03510
Gregory Eales <gregoeales@aol.com>
2020-05-14 22:46:40 +0100

    Update teeny_go.py

--- teeny_go/teeny_go.py
+++ teeny_go/teeny_go.py
@@ -4,6 +4,7 @@ import gym
 import argparse
 from matplotlib import pyplot as plt
 from copy import copy
+from copy import deepcopy
 import numpy as np
 import time
 
@@ -33,7 +34,7 @@ class TeenyGo(object):
         return go_env
 
     def copy_game(self, game):
-        return deepcopy(game)
+        return copy(game)
 
     def get_move(self, state, valid_moves):
         p = self.policy_network.forward(state).detach().numpy()
@@ -48,7 +49,7 @@ class TeenyGo(object):
     def get_winrate(self, x):
         pass
 
-    def mcts(self, game, width, depth):
+    def mcts(self, game, width, depth, first=True):
 
         # MCTS
 
@@ -59,12 +60,11 @@ class TeenyGo(object):
         # if depth is reached return V
 
 
-        x = game.get_state()[0:3]
+        x = game.get_state()[0:3].reshape([1, 3, 9, 9])
 
         if depth == 0:
             v = self.value_network.forward(x).detach().numpy()
-            return 
-
+            return v
 
         # get policy
         p = self.policy_network.forward(x).detach().numpy()
@@ -75,19 +75,19 @@ class TeenyGo(object):
         # get best moves
         moves = self.get_best_moves(p, width)
 
-
         # get simulated moves
         sims = self.get_simulated_games(game, moves)
 
         values = []
         # test each sim
         for sim in sims:
-            values.append(self.mcts(sim, width, depth-1))
+            values.append(self.mcts(sim, width, depth-1, first=False))
 
-        if depth==self.mcts_depth:
+        if first == False:
+            return np.sum(values)/len(values)
+
+        if first == True:
             return moves[np.argmax(values)]
-
-        return np.max(values)
 
     def get_best_moves(self, p, width):
         
@@ -99,14 +99,14 @@ class TeenyGo(object):
 
         return moves
 
-    def get_best_single_move(self, board):
+    def get_best_single_move(self, board, n_moves=10):
 
         state = board.get_state()[0:3].reshape([1, 3, 9, 9])
 
         valid_moves = board.get_valid_moves()
         p = self.policy_network.forward(state).detach().numpy()
         p = p*valid_moves
-        moves = self.get_best_moves(p, 10)
+        moves = self.get_best_moves(p, n_moves)
 
         print("Got moves")
         sims = []
@@ -138,7 +138,7 @@ def main():
     policy_net = PolicyNetwork(alpha = 0.001,num_res=3, num_channel=128)
     policy_net.load_state_dict(torch.load("PN-R3-C128-PFinal.pt", map_location={'cuda:0': 'cpu'}))
     value_net = ValueNetwork(alpha = 0.001, num_res=2, num_channel=64)
-    value_net.load_state_dict(torch.load("VN-R2-C64-V8.pt", map_location={'cuda:0': 'cpu'}))
+    value_net.load_state_dict(torch.load("VN-R2-C64-V4.pt", map_location={'cuda:0': 'cpu'}))
     teeny_go = TeenyGo(pn=policy_net, vn=value_net)
 
     parser = argparse.ArgumentParser(description='Demo Go Environment')
@@ -169,14 +169,26 @@ def main():
             print(value_net.forward(state))
             print("Time:", time.time()-t)
             """
+            """
+            state = go_env.get_state()[0:3].reshape([1, 3, 9, 9])
+            valid_moves = go_env.get_valid_moves()
+            action = teeny_go.get_move(state, valid_moves)
+            """
+            
+            
             t = time.time()
-            action = teeny_go.get_best_single_move(go_env)
+            action = teeny_go.get_best_single_move(go_env, n_moves=10)
             print("Teeny-Go Move:", action)
             print("Time:", time.time()-t)
+            
 
+            #action = teeny_go.mcts(go_env, 3, 3)
 
             try:
                 state, reward, done, _ = go_env.step(action)
+                s = state[0:3].reshape([1, 3, 9, 9])
+                print("Human Prospect:", value_net.forward(s).detach().numpy())
+
             except Exception as e:
                 print(e)
                 continue
@@ -186,6 +198,8 @@ def main():
                 action = go_env.uniform_random_action()
                 action = go_env.render("human")
                 state, reward, done, _ = go_env.step(action)
+                s = state[0:3].reshape([1, 3, 9, 9])
+                print("AI Prospect:", value_net.forward(s).detach().numpy())
 
 
         if go_env.get_winning() == 1:
