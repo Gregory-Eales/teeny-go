bd36c50f0844ed7fd7758a543c24a6e96181b7d1
Gregory Eales <gregoeales@aol.com>
2020-09-07 23:54:52 -0700

    Update

---
+++ GymGo-master/gym_go/envs/go_env.py
@@ -0,0 +1,261 @@
+from enum import Enum
+
+import gym
+import numpy as np
+
+from gym_go import govars, rendering
+from gym_go.gogame import GoGame
+
+
+class RewardMethod(Enum):
+    """
+    REAL: 0 = game is ongoing, 1 = black won, -1 = game tied or white won
+    HEURISTIC: If game is ongoing, the reward is the area difference between black and white.
+    Otherwise the game has ended, and if black has more area, the reward is BOARD_SIZE**2, otherwise it's -BOARD_SIZE**2
+    """
+    REAL = 'real'
+    HEURISTIC = 'heuristic'
+
+
+class GoEnv(gym.Env):
+    metadata = {'render.modes': ['terminal', 'human']}
+    gogame = GoGame()
+    govars = govars
+
+    def __init__(self, size, komi=0, reward_method='real'):
+        '''
+        @param reward_method: either 'heuristic' or 'real'
+        heuristic: gives # black pieces - # white pieces.
+        real: gives 0 for in-game move, 1 for winning, -1 for losing,
+            0 for draw, all from black player's perspective
+        '''
+        self.size = size
+        self.komi = komi
+        self.state = GoGame.get_init_board(size)
+        self.reward_method = RewardMethod(reward_method)
+        self.observation_space = gym.spaces.Box(np.float32(0), np.float32(govars.NUM_CHNLS),
+                                                shape=(govars.NUM_CHNLS, size, size))
+        self.action_space = gym.spaces.Discrete(GoGame.get_action_size(self.state))
+        self.group_map = [set(), set()]
+        self.done = False
+
+    def reset(self):
+        '''
+        Reset state, go_board, curr_player, prev_player_passed,
+        done, return state
+        '''
+        self.state = GoGame.get_init_board(self.size)
+        self.group_map = [set(), set()]
+        self.done = False
+        return np.copy(self.state)
+
+    def step(self, action):
+        '''
+        Assumes the correct player is making a move. Black goes first.
+        return observation, reward, done, info
+        '''
+        assert not self.done
+        if isinstance(action, tuple) or isinstance(action, list) or isinstance(action, np.ndarray):
+            assert 0 <= action[0] < self.size
+            assert 0 <= action[1] < self.size
+            action = self.size * action[0] + action[1]
+        elif action is None:
+            action = self.size ** 2
+
+        actions = np.array([action])
+        states, group_maps = GoGame.get_next_states(self.state, actions, self.group_map)
+        self.state, self.group_map = states[0], group_maps[0]
+        self.done = GoGame.get_game_ended(self.state)
+        return np.copy(self.state), self.get_reward(), self.done, self.get_info()
+
+    def game_ended(self):
+        return self.done
+
+    def turn(self):
+        return GoGame.get_turn(self.state)
+
+    def prev_player_passed(self):
+        return GoGame.get_prev_player_passed(self.state)
+
+    def get_valid_moves(self):
+        return GoGame.get_valid_moves(self.state)
+
+    def action_2d_to_1d(self, action_2d):
+        if action_2d is None:
+            action_1d = self.size ** 2
+        else:
+            action_1d = action_2d[0] * self.size + action_2d[1]
+        return action_1d
+
+    def uniform_random_action(self):
+        valid_moves = self.get_valid_moves()
+        valid_move_idcs = np.argwhere(valid_moves).flatten()
+        return np.random.choice(valid_move_idcs)
+
+    def get_info(self):
+        """
+        :return: Debugging info for the state
+        """
+        return {
+            'prev_player_passed': GoGame.get_prev_player_passed(self.state),
+            'turn': 'b' if GoGame.get_turn(self.state) == GoEnv.govars.BLACK else 'w',
+            'game_ended': GoGame.get_game_ended(self.state)
+        }
+
+    def get_state(self):
+        """
+        :return: copy of state
+        """
+        return np.copy(self.state)
+
+    def get_canonical_state(self):
+        """
+        :return: canonical shallow copy of state
+        """
+        return GoGame.get_canonical_form(self.state)
+
+    def get_canonical_group_map(self):
+        if self.turn() == govars.BLACK:
+            return self.group_map
+        else:
+            return list(reversed(self.group_map))
+
+    def get_children(self, canonical=False, padded=True):
+        """
+        :return: Same as get_children, but in canonical form
+        """
+        return GoGame.get_children(self.state, self.group_map, canonical, padded)
+
+    def get_winning(self):
+        """
+        :return: Who's currently winning in BLACK's perspective, regardless if the game is over
+        """
+        return GoGame.get_winning(self.state, self.komi)
+
+    def get_winner(self):
+        """
+        Get's the winner in BLACK's perspective
+        :return:
+        """
+
+        if self.game_ended():
+            return self.get_winning()
+        else:
+            return 0
+
+    def get_reward(self):
+        '''
+        Return reward based on reward_method.
+        heuristic: black total area - white total area
+        real: 0 for in-game move, 1 for winning, 0 for losing,
+            0.5 for draw, from black player's perspective.
+            Winning and losing based on the Area rule
+            Also known as Trump Taylor Scoring
+        Area rule definition: https://en.wikipedia.org/wiki/Rules_of_Go#End
+        '''
+        if self.reward_method == RewardMethod.REAL:
+            return self.get_winner()
+
+        elif self.reward_method == RewardMethod.HEURISTIC:
+            black_area, white_area = GoGame.get_areas(self.state)
+            area_difference = black_area - white_area
+            komi_correction = area_difference - self.komi
+            if self.game_ended():
+                return (1 if komi_correction > 0 else -1) * self.size ** 2
+            return komi_correction
+        else:
+            raise Exception("Unknown Reward Method")
+
+    def __str__(self):
+        return GoGame.str(self.state)
+
+    def close(self):
+        if hasattr(self, 'window'):
+            assert hasattr(self, 'pyglet')
+            self.window.close()
+            self.pyglet.app.exit()
+
+    def render(self, mode='terminal'):
+        if mode == 'terminal':
+            print(self.__str__())
+        elif mode == 'human':
+            import pyglet
+            from pyglet.window import mouse
+            from pyglet.window import key
+
+            screen = pyglet.canvas.get_display().get_default_screen()
+            window_width = int(min(screen.width, screen.height) * 2 / 3)
+            window_height = int(window_width * 1.2)
+            window = pyglet.window.Window(window_width, window_height)
+
+            self.window = window
+            self.pyglet = pyglet
+            self.user_action = None
+
+            # Set Cursor
+            cursor = window.get_system_mouse_cursor(window.CURSOR_CROSSHAIR)
+            window.set_mouse_cursor(cursor)
+
+            # Outlines
+            lower_grid_coord = window_width * 0.075
+            board_size = window_width * 0.85
+            upper_grid_coord = board_size + lower_grid_coord
+            delta = board_size / (self.size - 1)
+            piece_r = delta / 3.3  # radius
+
+            @window.event
+            def on_draw():
+                pyglet.gl.glClearColor(0.7, 0.5, 0.3, 1)
+                window.clear()
+
+                pyglet.gl.glLineWidth(3)
+                batch = pyglet.graphics.Batch()
+
+                # draw the grid and labels
+                rendering.draw_grid(batch, delta, self.size, lower_grid_coord, upper_grid_coord)
+
+                # info on top of the board
+                rendering.draw_info(batch, window_width, window_height, upper_grid_coord, self.state)
+
+                # Inform user what they can do
+                rendering.draw_command_labels(batch, window_width, window_height)
+
+                rendering.draw_title(batch, window_width, window_height)
+
+                batch.draw()
+
+                # draw the pieces
+                rendering.draw_pieces(batch, lower_grid_coord, delta, piece_r, self.size, self.state)
+
+            @window.event
+            def on_mouse_press(x, y, button, modifiers):
+                if button == mouse.LEFT:
+                    grid_x = (x - lower_grid_coord)
+                    grid_y = (y - lower_grid_coord)
+                    x_coord = round(grid_x / delta)
+                    y_coord = round(grid_y / delta)
+                    try:
+                        self.window.close()
+                        pyglet.app.exit()
+                        self.user_action = (x_coord, y_coord)
+                    except:
+                        pass
+
+            @window.event
+            def on_key_press(symbol, modifiers):
+                if symbol == key.P:
+                    self.window.close()
+                    pyglet.app.exit()
+                    self.user_action = None
+                elif symbol == key.R:
+                    self.reset()
+                    self.window.close()
+                    pyglet.app.exit()
+                elif symbol == key.E:
+                    self.window.close()
+                    pyglet.app.exit()
+                    self.user_action = -1
+
+            pyglet.app.run()
+
+            return self.user_action
---
+++ GymGo-master/gym_go/gogame.py
@@ -0,0 +1,461 @@
+import numpy as np
+from gym_go import state_utils, govars
+from scipy import ndimage
+from sklearn import preprocessing
+
+"""
+The state of the game is a numpy array
+* Are values are either 0 or 1
+
+* Shape [NUM_CHNLS, SIZE, SIZE]
+
+0 - Black pieces
+1 - White pieces
+2 - Turn (0 - black, 1 - white)
+3 - Invalid moves (including ko-protection)
+4 - Previous move was a pass
+5 - Game over
+"""
+
+
+class GoGame:
+
+    @staticmethod
+    def get_init_board(size):
+        # return initial board (numpy board)
+        state = np.zeros((govars.NUM_CHNLS, size, size))
+        return state
+
+    @staticmethod
+    def get_next_state(state, action1d, group_map=None, canonical=False):
+        action_wrap = np.array([action1d])
+        next_states_wrap, next_gmps_wrap = GoGame.get_next_states(state, action_wrap, group_map, canonical)
+        assert len(next_states_wrap) == 1
+        assert len(next_gmps_wrap) == 1
+
+        return next_states_wrap[0], next_gmps_wrap[0]
+
+    @staticmethod
+    def get_next_states(state, batch_action1d, group_map=None, canonical=False):
+        """
+        Does not change the given state
+        """
+        if group_map is None:
+            group_map = state_utils.get_group_map(state)
+
+        m, n = state_utils.get_board_size(state)
+        pass_idcs = np.where(batch_action1d == m * n)
+        non_pass_idcs = np.where(batch_action1d != m * n)
+
+        batch_size = len(batch_action1d)
+        board_shape = state.shape[1:]
+        batch_action2d = np.empty((batch_size, 2), dtype=np.int)
+        batch_action2d[:, 0] = batch_action1d // n
+        batch_action2d[:, 1] = batch_action1d % n
+        batch_action2d[np.where(batch_action1d == m * n)] = 0
+
+        player = state_utils.get_turn(state)
+        opponent = 1 - player
+        previously_passed = GoGame.get_prev_player_passed(state)
+
+        batch_states = np.tile(state, (batch_size, 1, 1, 1))
+
+        # Check move is valid
+        assert (batch_states[non_pass_idcs, govars.INVD_CHNL, batch_action2d[non_pass_idcs, 0], batch_action2d[
+            non_pass_idcs, 1]] == 0).all(), "Invalid move"
+
+        batch_group_maps = [[group_map[0].copy(), group_map[1].copy()] for _ in range(batch_size)]
+        batch_single_kill = [None for _ in range(batch_size)]
+        batch_killed_groups = [set() for _ in range(batch_size)]
+
+        batch_adj_locs, batch_surrounded = state_utils.get_batch_adj_data(state, batch_action2d)
+
+        if previously_passed:
+            batch_states[pass_idcs, govars.DONE_CHNL] = 1
+        else:
+            batch_states[pass_idcs, govars.PASS_CHNL] = 1
+
+        # Non passes
+        batch_states[non_pass_idcs, govars.PASS_CHNL] = 0
+
+        # Add pieces
+        batch_states[non_pass_idcs, player, batch_action2d[non_pass_idcs, 0], batch_action2d[non_pass_idcs, 1]] = 1
+
+        batch_data = enumerate(zip(batch_action1d, batch_action2d, batch_states, batch_group_maps, batch_adj_locs,
+                                   batch_killed_groups))
+
+        for i, (action_1d, action_2d, state, group_map, adj_locs, killed_groups) in batch_data:
+            # if the current player passes
+            if action_1d == m * n:
+                continue
+
+            action_2d = tuple(action_2d)
+
+            # Get all adjacent information
+            adj_own_groups, adj_opp_groups = state_utils.get_adjacent_groups(group_map, adj_locs, player)
+
+            # Go through opponent groups
+            for group in adj_opp_groups:
+                assert action_2d in group.liberties, (action_2d, player, group, state)
+                if len(group.liberties) <= 1:
+                    # Killed group
+                    killed_groups.add(group)
+
+                    # Remove group in board and group map
+                    for loc in group.locations:
+                        state[opponent, loc[0], loc[1]] = 0
+                    group_map[opponent].remove(group)
+
+                    # Metric for ko-protection
+                    if len(group.locations) <= 1 and batch_single_kill[i] is None:
+                        batch_single_kill[i] = next(iter(group.locations))
+
+            adj_opp_groups.difference_update(killed_groups)
+
+            # Update surviving adjacent opponent groups by removing liberties by the new action
+            for opp_group in adj_opp_groups:
+                assert action_2d in opp_group.liberties, (action_2d, opp_group, adj_opp_groups)
+
+                # New group copy
+                group_map[opponent].remove(opp_group)
+                opp_group = opp_group.copy()
+                group_map[opponent].add(opp_group)
+
+                opp_group.liberties.remove(action_2d)
+
+            # Update adjacent own groups that are merged with the action
+            if len(adj_own_groups) > 0:
+                merged_group = adj_own_groups.pop()
+                group_map[player].remove(merged_group)
+                merged_group = merged_group.copy()
+            else:
+                merged_group = govars.Group()
+
+            group_map[player].add(merged_group)
+
+            # Locations from action and adjacent groups
+            merged_group.locations.add(action_2d)
+
+            for own_group in adj_own_groups:
+                merged_group.locations.update(own_group.locations)
+                merged_group.liberties.update(own_group.liberties)
+                group_map[player].remove(own_group)
+
+            # Liberties from action
+            for adj_loc in adj_locs:
+                if np.count_nonzero(state[[govars.BLACK, govars.WHITE], adj_loc[0], adj_loc[1]]) == 0:
+                    merged_group.liberties.add(adj_loc)
+
+            if action_2d in merged_group.liberties:
+                merged_group.liberties.remove(action_2d)
+
+            # More work to do if we killed
+            if len(killed_groups) > 0:
+                killed_map = np.zeros(board_shape)
+                for group in killed_groups:
+                    for loc in group.locations:
+                        killed_map[loc] = 1
+                # Update own groups adjacent to opponent groups that we just killed
+                killed_liberties = ndimage.binary_dilation(killed_map)
+                affected_idcs = set(zip(*np.nonzero(state[player] * killed_liberties)))
+                groups_to_update = set()
+                for group in group_map[player]:
+                    if not affected_idcs.isdisjoint(group.locations):
+                        groups_to_update.add(group)
+
+                all_pieces = np.sum(state[[govars.BLACK, govars.WHITE]], axis=0)
+                empties = (1 - all_pieces)
+                for group in groups_to_update:
+                    group_matrix = np.zeros(board_shape)
+                    for loc in group.locations:
+                        group_matrix[loc] = 1
+
+                    additional_liberties = ndimage.binary_dilation(group_matrix) * empties * killed_map
+                    additional_liberties = set(zip(*np.where(additional_liberties)))
+
+                    group_map[player].remove(group)
+                    group = group.copy()
+                    group_map[player].add(group)
+
+                    group.liberties.update(additional_liberties)
+
+        # Update illegal moves
+        batch_states[:, govars.INVD_CHNL] = state_utils.get_batch_invalid_moves(batch_states, batch_group_maps, player)
+
+        # If group was one piece, and location is surrounded by opponents,
+        # activate ko protection
+        for i, (single_kill, killed_groups, surrounded) in enumerate(zip(batch_single_kill, batch_killed_groups,
+                                                                         batch_surrounded)):
+            if single_kill is not None and len(killed_groups) == 1 and surrounded:
+                state[govars.INVD_CHNL, single_kill[0], single_kill[1]] = 1
+
+        # Switch turn
+        state_utils.batch_set_turn(batch_states)
+
+        if canonical:
+            GoGame.set_batch_canonical_form(batch_states, batch_group_maps, opponent)
+
+        return batch_states, batch_group_maps
+
+    @staticmethod
+    def get_children(state, group_map=None, canonical=False, padded=True):
+        if group_map is None:
+            group_map = state_utils.get_group_map(state)
+
+        valid_moves = GoGame.get_valid_moves(state)
+        n = len(valid_moves)
+        valid_move_idcs = np.argwhere(valid_moves).flatten()
+        children, child_group_maps = GoGame.get_next_states(state, valid_move_idcs, group_map, canonical)
+        if padded:
+            padded_children = np.zeros((n, *state.shape))
+            padded_group_maps = np.empty(n, dtype=np.object)
+
+            padded_children[valid_move_idcs] = children
+            padded_group_maps[valid_move_idcs] = child_group_maps
+
+            children = padded_children
+            child_group_maps = padded_group_maps
+        return children, child_group_maps
+
+    @staticmethod
+    def get_action_size(state=None, board_size: int = None):
+        # return number of actions
+        if state is not None:
+            m, n = state_utils.get_board_size(state)
+        elif board_size is not None:
+            m, n = board_size, board_size
+        else:
+            raise RuntimeError('No argument passed')
+        return m * n + 1
+
+    @staticmethod
+    def get_prev_player_passed(state):
+        m, n = state_utils.get_board_size(state)
+        return np.count_nonzero(state[govars.PASS_CHNL] == 1) == m * n
+
+    @staticmethod
+    def get_game_ended(state):
+        """
+        :param state:
+        :return: 0/1 = game not ended / game ended respectively
+        """
+        m, n = state_utils.get_board_size(state)
+        return int(np.count_nonzero(state[govars.DONE_CHNL] == 1) == m * n)
+
+    @staticmethod
+    def get_winning(state, komi=0):
+        black_area, white_area = GoGame.get_areas(state)
+        area_difference = black_area - white_area
+        komi_correction = area_difference - komi
+
+        if komi_correction > 0:
+            return 1
+        elif komi_correction == 0:
+            return 0
+        else:
+            assert komi_correction < 0
+            return -1
+
+    @staticmethod
+    def get_turn(state):
+        """
+        :param state:
+        :return: Who's turn it is (govars.BLACK/govars.WHITE)
+        """
+        return state_utils.get_turn(state)
+
+    @staticmethod
+    def get_valid_moves(state):
+        # return a fixed size binary vector
+        if GoGame.get_game_ended(state):
+            return np.zeros(GoGame.get_action_size(state))
+        return np.append(1 - state[govars.INVD_CHNL].flatten(), 1)
+
+    @staticmethod
+    def action_2d_to_1d(action_2d, state):
+        size = state.shape[1]
+        if action_2d is None:
+            action_1d = size ** 2
+        else:
+            action_1d = action_2d[0] * size + action_2d[1]
+        return action_1d
+
+    @staticmethod
+    def get_liberties(state: np.ndarray):
+        return state_utils.get_liberties(state)
+
+    @staticmethod
+    def get_num_liberties(state: np.ndarray):
+        return state_utils.get_num_liberties(state)
+
+    @staticmethod
+    def get_areas(state):
+        '''
+        Return black area, white area
+        Use DFS helper to find territory.
+        '''
+
+        all_pieces = np.sum(state[[govars.BLACK, govars.WHITE]], axis=0)
+        empties = 1 - all_pieces
+
+        empty_labels, num_empty_areas = ndimage.measurements.label(empties)
+
+        black_area, white_area = np.sum(state[govars.BLACK]), np.sum(state[govars.WHITE])
+        for label in range(1, num_empty_areas + 1):
+            empty_area = empty_labels == label
+            neighbors = ndimage.binary_dilation(empty_area)
+            black_claim = False
+            white_claim = False
+            if (state[govars.BLACK] * neighbors > 0).any():
+                black_claim = True
+            if (state[govars.WHITE] * neighbors > 0).any():
+                white_claim = True
+            if black_claim and not white_claim:
+                black_area += np.sum(empty_area)
+            elif white_claim and not black_claim:
+                white_area += np.sum(empty_area)
+
+        return black_area, white_area
+
+    @staticmethod
+    def get_canonical_form(state):
+        """
+        The returned state is a shallow copy of the given state
+        :param state:
+        :param player:
+        :return:
+        """
+
+        player = GoGame.get_turn(state)
+        if player == govars.BLACK:
+            return state
+        else:
+            assert player == govars.WHITE
+            num_channels = state.shape[0]
+            channels = np.arange(num_channels)
+            channels[govars.BLACK] = govars.WHITE
+            channels[govars.WHITE] = govars.BLACK
+            can_state = state[channels]
+            state_utils.set_turn(can_state)
+            return can_state
+
+    @staticmethod
+    def set_batch_canonical_form(batch_states, batch_group_maps, player):
+        """
+        Assumes the turn of all states is player
+        The returned state is a seperate copy of the given state
+        :param batch_states:
+        :param player:
+        :return:
+        """
+
+        if player == govars.WHITE:
+            batch_states[:, [govars.BLACK, govars.WHITE]] = batch_states[:, [govars.WHITE, govars.BLACK]]
+            state_utils.batch_set_turn(batch_states)
+            for group_map in batch_group_maps:
+                group_map.reverse()
+
+    @staticmethod
+    def random_symmetry(chunk):
+        """
+        Returns a random symmetry of the chunk
+        :param chunk: A (C, BOARD_SIZE, BOARD_SIZE) numpy array, where C is any number
+        :return:
+        """
+        orientation = np.random.randint(0, 8)
+
+        if (orientation >> 0) % 2:
+            # Horizontal flip
+            chunk = np.flip(chunk, 2)
+        if (orientation >> 1) % 2:
+            # Vertical flip
+            chunk = np.flip(chunk, 1)
+        if (orientation >> 2) % 2:
+            # Rotate 90 degrees
+            chunk = np.rot90(chunk, axes=(1, 2))
+
+        return chunk
+
+    @staticmethod
+    def get_symmetries(chunk):
+        """
+        :param chunk: A (C, BOARD_SIZE, BOARD_SIZE) numpy array, where C is any number
+        :return: All 8 orientations that are symmetrical in a Go game over the 2nd and 3rd axes
+        (i.e. rotations, flipping and combos of them)
+        """
+        symmetries = []
+
+        for i in range(8):
+            x = chunk
+            if (i >> 0) % 2:
+                # Horizontal flip
+                x = np.flip(x, 2)
+            if (i >> 1) % 2:
+                # Vertical flip
+                x = np.flip(x, 1)
+            if (i >> 2) % 2:
+                # Rotation 90 degrees
+                x = np.rot90(x, axes=(1, 2))
+            symmetries.append(x)
+
+        return symmetries
+
+    @staticmethod
+    def random_weighted_action(move_weights):
+        """
+        Assumes all invalid moves have weight 0
+        Action is 1D
+        Expected shape is (NUM OF MOVES, )
+        """
+        move_weights = preprocessing.normalize(move_weights[np.newaxis], norm='l1')
+        return np.random.choice(np.arange(len(move_weights[0])), p=move_weights[0])
+
+    @staticmethod
+    def random_action(state):
+        """
+        Assumed to be (NUM_CHNLS, BOARD_SIZE, BOARD_SIZE)
+        Action is 1D
+        """
+        invalid_moves = state[govars.INVD_CHNL].flatten()
+        invalid_moves = np.append(invalid_moves, 0)
+        move_weights = 1 - invalid_moves
+
+        return GoGame.random_weighted_action(move_weights)
+
+    @staticmethod
+    def str(state):
+        board_str = ' '
+
+        size = state.shape[1]
+        for i in range(size):
+            board_str += '   {}'.format(i)
+        board_str += '\n  '
+        board_str += '----' * size + '-'
+        board_str += '\n'
+        for i in range(size):
+            board_str += '{} |'.format(i)
+            for j in range(size):
+                if state[0, i, j] == 1:
+                    board_str += ' B'
+                elif state[1, i, j] == 1:
+                    board_str += ' W'
+                elif state[2, i, j] == 1:
+                    board_str += ' .'
+                else:
+                    board_str += '  '
+
+                board_str += ' |'
+
+            board_str += '\n  '
+            board_str += '----' * size + '-'
+            board_str += '\n'
+
+        black_area, white_area = GoGame.get_areas(state)
+        game_ended = GoGame.get_game_ended(state)
+        prev_player_passed = GoGame.get_prev_player_passed(state)
+        turn = GoGame.get_turn(state)
+        board_str += '\tTurn: {}, Last Turn Passed: {}, Game Over: {}\n'.format('B' if turn == 0 else 'W',
+                                                                                prev_player_passed,
+                                                                                game_ended)
+        board_str += '\tBlack Area: {}, White Area: {}\n'.format(black_area, white_area)
+        return board_str
